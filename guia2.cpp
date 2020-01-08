323a6a0fdc3319fc554712856b0d25549d6fd01d
luis19astu99 <59629792+luis19astu99@users.noreply.github.com>
2020-01-08 08:50:54 -0500

    Update guia2.cpp

--- guia2.cpp
+++ guia2.cpp
@@ -1,11 +1,15 @@
-#include <stdio.h> #include <stdlib.h>
-#include <pthread.h> //crear funciones #include <unistd.h> // libreria para sleep #include<windows.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h> //crear funciones 
+#include <unistd.h> // libreria para sleep 
+#include<windows.h>
 #include <string.h>
 
 void *verde(void*); void *naranja(void*); void *rojo(void*);
 void *verde(void *tid)
 {
-	system("cls"); //cls o clear es para limpiar la pantalla de comandos system("color 20");
+	system("cls"); //cls o clear es para limpiar la pantalla de comandos 
+	system("color 20");
 	Sleep(3000);
 }
 void *naranja(void *tid)
