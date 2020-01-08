176b149872ccfaf3ec8d64176430d0e6f61f1afc
alexander1915 <52326373+alexander1915@users.noreply.github.com>
2020-01-08 08:59:20 -0500

    Update guia2.cpp

--- guia2.cpp
+++ guia2.cpp
@@ -7,21 +7,31 @@
 void *verde(void*); void *naranja(void*); void *rojo(void*);
 void *verde(void *tid)
 {
+	int verdes;
+	printf( "Introduzca el tiempo de color verde " );
+	scanf( "%d", &verdes);
 	system("cls"); //cls o clear es para limpiar la pantalla de comandos
 	system("color 20");
-	Sleep(3000);
+	Sleep(verdes);
 }
 void *naranja(void *tid)
 {
-	system("cls"); system("color E0"); Sleep(3000);
+	int amarillo;
+	printf( "Introduzca el tiempo de color naranja " );
+	scanf( "%d", &amarillo);
+	system("cls"); system("color E0"); Sleep(amarillo);
 }
 void *rojo(void *tid)
 {
-	system("cls"); system("color 40"); Sleep(3000);
+	int rojos;
+	printf( "Introduzca el tiempo de color rojo " );
+	scanf( "%d", &rojos);
+	system("cls"); system("color 40"); Sleep(1000);
 }
 int main()
 {
 	int i = 1;
+		
 	pthread_t pthread_rojo, pthread_naranja, pthread_verde;
 	pthread_create (&pthread_verde, NULL, verde, (void *)i); pthread_join(pthread_verde, NULL);
 	pthread_create (&pthread_naranja,NULL, naranja, (void *)i); pthread_join(pthread_naranja, NULL);
