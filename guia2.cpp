bfc75191e94e428e9544ca5f98a0fd242e8670e4
alexander1915 <52326373+alexander1915@users.noreply.github.com>
2020-01-08 09:42:06 -0500

    Update guia2.cpp

--- guia2.cpp
+++ guia2.cpp
@@ -1,42 +1,28 @@
-#include <stdio.h> #include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h>
 #include <pthread.h> //crear funciones
 #include <unistd.h> // libreria para sleep
 #include <windows.h>
 #include <string.h>
 
 void *verde(void*); void *naranja(void*); void *rojo(void*);
-void *verde(void *tid)
-{
-	int verdes;
-	printf( "Introduzca el tiempo de color verde " );
-	scanf( "%d", &verdes);
-	system("cls"); //cls o clear es para limpiar la pantalla de comandos
-	system("color 20");
-	Sleep(verdes);
-}
-void *naranja(void *tid)
-{
-	int amarillo;
-	printf( "Introduzca el tiempo de color naranja " );
-	scanf( "%d", &amarillo);
-	system("cls"); system("color E0"); Sleep(amarillo);
-}
-void *rojo(void *tid)
-{
-	int rojos;
-	printf( "Introduzca el tiempo de color rojo " );
-	scanf( "%d", &rojos);
-	system("cls"); system("color 40"); Sleep(1000);
-}
+
 int main()
 {
 	int i = 1;
-		
+	int ver,	rj,amarillo;
+	printf( "Introduzca el tiempo de color verde " );
+	scanf( "%d", &ver);
+	printf( "Introduzca el tiempo de color rojo " );
+	scanf( "%d", &rj);
+	printf( "Introduzca el tiempo de color naranja " );
+	scanf( "%d", &amarillo);
+	
+	system("cls"); system("color 20"); Sleep(ver);	
+	system("cls"); system("color E0"); Sleep(amarillo);		
+	system("cls"); system("color 40"); Sleep(rj);
 	pthread_t pthread_rojo, pthread_naranja, pthread_verde;
-	pthread_create (&pthread_verde, NULL, verde, (void *)i); pthread_join(pthread_verde, NULL);
-	pthread_create (&pthread_naranja,NULL, naranja, (void *)i); pthread_join(pthread_naranja, NULL);
-	pthread_create (&pthread_rojo, NULL, rojo, (void *)i); pthread_join(pthread_rojo, NULL);
-	pthread_exit(NULL);
 	return 0;
 }
+
 
